dc7ef4bb47fd7b294a6095ea80d68e4ef7e35017
CrystalHareSign <wojtek123b@onet.eu>
2025-02-09 21:29:12 +0100

    Interakcje v1

---
+++ WarzywoGamig/Assets/Interactions/InteractivityManager.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractivityManager : MonoBehaviour
+{
+    public static InteractivityManager Instance;
+
+    private Dictionary<GameObject, bool> interactivityStatus = new Dictionary<GameObject, bool>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Upewnij siê, ¿e obiekt nie zostanie zniszczony przy zmianie sceny
+            Debug.Log("InteractivityManager initialized.");
+        }
+        else
+        {
+            Debug.LogWarning("Another instance of InteractivityManager found. Destroying this instance.");
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void RegisterInteractable(GameObject interactable, bool alwaysInteractive)
+    {
+        if (!interactivityStatus.ContainsKey(interactable))
+        {
+            interactivityStatus[interactable] = alwaysInteractive;
+            Debug.Log($"Registered interactable: {interactable.name}, always interactive: {alwaysInteractive}");
+        }
+    }
+
+    public void UpdateInteractivityStatus(GameObject interactable, bool isInteractive)
+    {
+        if (interactivityStatus.ContainsKey(interactable))
+        {
+            // Nie aktualizujemy statusu interaktywnoœci jeœli obiekt jest zawsze interaktywny
+            if (!interactivityStatus[interactable])
+            {
+                interactivityStatus[interactable] = isInteractive;
+                Debug.Log($"Updated interactivity status for {interactable.name} to {isInteractive}");
+            }
+        }
+    }
+
+    public bool IsInteractable(GameObject interactable)
+    {
+        if (interactivityStatus.ContainsKey(interactable))
+        {
+            return interactivityStatus[interactable];
+        }
+        return false;
+    }
+
+    public void RestoreInteractivity(GameObject interactable)
+    {
+        if (interactivityStatus.ContainsKey(interactable))
+        {
+            interactivityStatus[interactable] = true;
+            var hoverMessage = interactable.GetComponent<HoverMessage>();
+            if (hoverMessage != null)
+            {
+                hoverMessage.isInteracted = false;
+            }
+            Debug.Log($"Restored interactivity for {interactable.name}");
+        }
+    }
+}
---
+++ WarzywoGamig/Assets/Player/PlayerInteraction.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float interactionRange = 4f; // Zasiêg interakcji
+    public LayerMask interactableLayer; // Warstwa interaktywnych obiektów (ustaw w inspektorze)
+    public Camera playerCamera;
+    public Image progressCircle;
+    public TMP_Text messageText; // Odniesienie do komponentu TMP_Text
+
+    private float interactionTimer = 0f; // Licznik czasu trzymania przycisku
+    private InteractableItem currentInteractableItem = null; // Aktualnie celowany obiekt interaktywny
+    private float requiredHoldTime = 5f; // Domyœlny czas trzymania przycisku interakcji w sekundach
+
+    void Update()
+    {
+        // Rzutowanie promienia z pozycji gracza do przodu
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactionRange, interactableLayer))
+        {
+            InteractableItem interactableItem = hit.collider.GetComponent<InteractableItem>();
+            if (interactableItem != null && !interactableItem.hoverMessage.isInteracted)
+            {
+                // Wyœwietl komunikat po najechaniu kursorem na obiekt
+                if (messageText != null && interactableItem.hoverMessage != null)
+                {
+                    messageText.text = interactableItem.hoverMessage.message;
+                    messageText.gameObject.SetActive(true);
+                }
+
+                if (progressCircle != null)
+                {
+                    progressCircle.gameObject.SetActive(true);
+                }
+
+                // SprawdŸ, czy gracz trzyma przycisk interakcji
+                if (Input.GetKey(KeyCode.E))
+                {
+                    if (currentInteractableItem == interactableItem)
+                    {
+                        // Ustaw wymagany czas trzymania przycisku dla aktualnego obiektu interaktywnego
+                        requiredHoldTime = currentInteractableItem.requiredHoldTime;
+
+                        interactionTimer += Time.deltaTime;
+                        if (progressCircle != null)
+                        {
+                            progressCircle.fillAmount = interactionTimer / requiredHoldTime;
+                        }
+
+                        if (interactionTimer >= requiredHoldTime)
+                        {
+                            InteractWithObject(currentInteractableItem);
+                            interactionTimer = 0f;
+                            HideUI();
+                        }
+                    }
+                    else
+                    {
+                        currentInteractableItem = interactableItem;
+                        interactionTimer = 0f;
+                        if (progressCircle != null)
+                        {
+                            progressCircle.fillAmount = 0f;
+                        }
+                    }
+                }
+                else
+                {
+                    interactionTimer = 0f;
+                    if (progressCircle != null)
+                    {
+                        progressCircle.fillAmount = 0f;
+                    }
+                }
+            }
+            else
+            {
+                ResetInteraction();
+            }
+        }
+        else
+        {
+            ResetInteraction();
+        }
+    }
+
+    private void InteractWithObject(InteractableItem interactableItem)
+    {
+        if (interactableItem != null)
+        {
+            interactableItem.Interact();
+        }
+    }
+
+    private void HideUI()
+    {
+        if (progressCircle != null)
+        {
+            progressCircle.gameObject.SetActive(false);
+        }
+
+        if (messageText != null)
+        {
+            messageText.gameObject.SetActive(false);
+        }
+    }
+
+    private void ResetInteraction()
+    {
+        interactionTimer = 0f;
+        if (progressCircle != null)
+        {
+            progressCircle.fillAmount = 0f;
+            progressCircle.gameObject.SetActive(false);
+        }
+
+        if (messageText != null)
+        {
+            messageText.gameObject.SetActive(false);
+        }
+
+        currentInteractableItem = null;
+    }
+}
