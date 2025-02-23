8738f7e728019036af6ab11b1a47af9f50965968
CrystalHareSign <wojtek123b@onet.eu>
2025-02-23 22:10:13 +0100

    Building v0.5

---
+++ WarzywoGamig/Assets/Building/PrefabSize.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PrefabSize : MonoBehaviour
+{
+    [Tooltip("X")]
+    public int widthInTiles = 1;  // Liczba kafelków w poziomie (X)
+    [Tooltip("Z")]
+    public int depthInTiles = 1;  // Liczba kafelków w g³êbokoœci (Z)
+}
