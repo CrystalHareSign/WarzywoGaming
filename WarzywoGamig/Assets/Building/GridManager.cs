7960ea77b7bb6cb0d412d0c5e3ec222f30b653e4
CrystalHareSign <wojtek123b@onet.eu>
2025-02-24 21:23:30 +0100

    Building v1

--- WarzywoGamig/Assets/Building/GridManager.cs
+++ WarzywoGamig/Assets/Building/GridManager.cs
@@ -2,16 +2,14 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    public GameObject objectPrefab; // Prefab do podgl¹du i finalnego postawienia
     public float gridSize = 1f; // Rozmiar siatki
     public float tileSpacing = 0.1f; // Odstêp miêdzy kafelkami
     public Transform gridArea; // Obszar siatki
     public GameObject gridTilePrefab; // Prefab kafelka siatki
-    public Material whiteMaterial; // Materia³ siatki
+    public GameObject objectPreviewPrefab; // Prefab podgl¹du budowy
 
-    private GameObject previewObject; // Obiekt podgl¹du
     private bool isBuildingMode = false; // Tryb budowy w³¹czony/wy³¹czony
-    private GameObject[] gridTiles; // Tablica kafelków
+    private GameObject previewObject; // Obiekt podgl¹du
     private float gridAreaWidth;
     private float gridAreaHeight;
 
@@ -24,105 +22,95 @@ public class GridManager : MonoBehaviour
 
     void Update()
     {
-        // W³¹cz/wy³¹cz tryb budowy za pomoc¹ B
         if (Input.GetKeyDown(KeyCode.B))
         {
             isBuildingMode = !isBuildingMode;
             ToggleGridVisibility(isBuildingMode);
 
-            if (isBuildingMode && previewObject == null)
-            {
+            if (isBuildingMode)
                 CreatePreviewObject();
-            }
-
-            if (!isBuildingMode && previewObject != null)
-            {
-                Destroy(previewObject);
-                previewObject = null; // Usuwamy obiekt podgl¹du, gdy tryb budowy jest wy³¹czony
-            }
+            else
+                DestroyPreviewObject();
         }
 
-        // Tryb budowy
         if (isBuildingMode && previewObject != null)
         {
-            MovePreviewObject();
+            Vector3 mousePosition = GetMouseWorldPosition();
+            previewObject.transform.position = SnapToGrid(mousePosition);
 
-            if (Input.GetMouseButtonDown(0)) // Klikniêcie lewego przycisku myszy
-            {
+            if (Input.GetMouseButtonDown(0))
                 PlaceObject();
-            }
         }
     }
-
-    // Tworzy obiekt podgl¹du do snappowania na siatce
-    void MovePreviewObject()
+    // Snappowanie pozycji do siatki uwzglêdniaj¹c tileSpacing
+    public Vector3 SnapToGrid(Vector3 position)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        // Uwzglêdniamy tileSpacing przy obliczeniach pozycji
+        float snappedX = Mathf.Floor((position.x - gridArea.position.x + (gridSize / 2) + (tileSpacing / 2)) / (gridSize + tileSpacing)) * (gridSize + tileSpacing) + gridArea.position.x;
+        float snappedZ = Mathf.Floor((position.z - gridArea.position.z + (gridSize / 2) + (tileSpacing / 2)) / (gridSize + tileSpacing)) * (gridSize + tileSpacing) + gridArea.position.z;
 
-        if (Physics.Raycast(ray, out hit))
-        {
-            Vector3 snappedPosition = SnapToGrid(hit.point);
-            previewObject.transform.position = snappedPosition;
-            previewObject.SetActive(true); // Aktywujemy obiekt podgl¹du
-
-            // Debugowanie
-            Debug.Log("Snapped Position: " + snappedPosition);
-        }
-    }
-
-    void CreatePreviewObject()
-    {
-        previewObject = Instantiate(objectPrefab);
-        previewObject.SetActive(false); // Na pocz¹tku obiekt jest niewidoczny
-
-        // Wy³¹czamy fizykê i collider
-        Collider col = previewObject.GetComponent<Collider>();
-        if (col != null) col.enabled = false;
-
-        Rigidbody rb = previewObject.GetComponent<Rigidbody>();
-        if (rb != null) rb.isKinematic = true; // Sprawiamy, ¿e nie ma fizyki
-
-        // Upewnij siê, ¿e obiekt podgl¹du nie wp³ywa na gracza
-        previewObject.GetComponent<Collider>().enabled = false;
-    }
-
-    // Umieszczenie finalnego obiektu w miejscu podgl¹du
-    void PlaceObject()
-    {
-        if (previewObject != null)
-        {
-            Vector3 placePosition = previewObject.transform.position;
-            GameObject placedObject = Instantiate(objectPrefab, placePosition, Quaternion.identity);
-
-            // W³¹czamy fizykê w nowo postawionym obiekcie
-            Collider col = placedObject.GetComponent<Collider>();
-            if (col != null) col.enabled = true;
-
-            Rigidbody rb = placedObject.GetComponent<Rigidbody>();
-            if (rb != null) rb.isKinematic = false; // Przywracamy fizykê dla postawionego obiektu
-        }
-    }
-
-    // Snappowanie pozycji do siatki
-    Vector3 SnapToGrid(Vector3 position)
-    {
-        float snappedX = Mathf.Round(position.x / gridSize) * gridSize;
-        float snappedZ = Mathf.Round(position.z / gridSize) * gridSize;
+        // Ustawiamy wysokoœæ na poziomie siatki
         float gridHeight = gridArea.position.y;
 
+        // Zwracamy now¹ skorygowan¹ pozycjê
         return new Vector3(snappedX, gridHeight, snappedZ);
     }
 
-    // Tworzenie siatki
+    // Pobiera pozycjê myszy w œwiecie
+    private Vector3 GetMouseWorldPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f))
+        {
+            return hit.point;
+        }
+        return Vector3.zero;
+    }
+
+    // Tworzy podgl¹d budowanego obiektu
+    private void CreatePreviewObject()
+    {
+        if (objectPreviewPrefab == null) return;
+
+        previewObject = Instantiate(objectPreviewPrefab);
+        DisableColliders(previewObject);
+    }
+
+    // Usuwa podgl¹d obiektu
+    private void DestroyPreviewObject()
+    {
+        if (previewObject != null)
+        {
+            Destroy(previewObject);
+        }
+    }
+
+    // Umieszcza obiekt na siatce
+    private void PlaceObject()
+    {
+        if (previewObject != null)
+        {
+            Instantiate(objectPreviewPrefab, previewObject.transform.position, Quaternion.identity);
+        }
+    }
+
+    // Wy³¹cza wszystkie collidery w obiekcie i jego childach
+    private void DisableColliders(GameObject obj)
+    {
+        Collider[] colliders = obj.GetComponentsInChildren<Collider>();
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = false;
+        }
+    }
+
+    // Tworzenie siatki z bia³ymi kafelkami
     private void CreateGrid()
     {
         if (gridTilePrefab == null) return;
 
         int gridWidth = Mathf.FloorToInt(gridAreaWidth / (gridSize + tileSpacing));
         int gridHeight = Mathf.FloorToInt(gridAreaHeight / (gridSize + tileSpacing));
-
-        gridTiles = new GameObject[gridWidth * gridHeight];
 
         Vector3 startPosition = gridArea.position - new Vector3(gridAreaWidth / 2, 0, gridAreaHeight / 2);
 
@@ -131,14 +119,15 @@ public class GridManager : MonoBehaviour
             for (int z = 0; z < gridHeight; z++)
             {
                 Vector3 tilePosition = new Vector3(x * (gridSize + tileSpacing), 0, z * (gridSize + tileSpacing)) + startPosition;
-                int index = x + z * gridWidth;
-                gridTiles[index] = Instantiate(gridTilePrefab, tilePosition, Quaternion.identity);
-                gridTiles[index].transform.parent = gridArea;
-                gridTiles[index].SetActive(false);
+                GameObject tile = Instantiate(gridTilePrefab, tilePosition, Quaternion.identity);
+                tile.transform.parent = gridArea;
+                tile.SetActive(false);
 
-                if (whiteMaterial != null)
+                // Ustawienie koloru kafelka na bia³y
+                Renderer tileRenderer = tile.GetComponent<Renderer>();
+                if (tileRenderer != null)
                 {
-                    gridTiles[index].GetComponent<Renderer>().material = whiteMaterial;
+                    tileRenderer.material.color = Color.white;
                 }
             }
         }
@@ -147,18 +136,9 @@ public class GridManager : MonoBehaviour
     // W³¹czanie/wy³¹czanie widocznoœci siatki
     private void ToggleGridVisibility(bool isVisible)
     {
-        foreach (GameObject tile in gridTiles)
+        foreach (Transform child in gridArea)
         {
-            tile.SetActive(isVisible);
-        }
-    }
-
-    // Przypisanie nowego obiektu do budowy
-    public void SetObjectPrefab(GameObject newPrefab)
-    {
-        if (newPrefab != null)
-        {
-            objectPrefab = newPrefab;
+            child.gameObject.SetActive(isVisible);
         }
     }
 }
