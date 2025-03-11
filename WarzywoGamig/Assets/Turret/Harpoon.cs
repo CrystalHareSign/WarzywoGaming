a2e21b91a1bf8d7ca097bd314259d3fdbbb3296f
CrystalHareSign <wojtek123b@onet.eu>
2025-03-11 21:25:33 +0100

    Turret v2.6 HarpScale

--- WarzywoGamig/Assets/Turret/Harpoon.cs
+++ WarzywoGamig/Assets/Turret/Harpoon.cs
@@ -4,7 +4,7 @@ public class Harpoon : MonoBehaviour
 {
     private Rigidbody harpoonRb;
     private HarpoonController harpoonController;
-    public float spawnedObjectSpeed;
+    public Transform treasureMountPoint; // Nowy punkt montażu dla Treasure
 
     private void Start()
     {
@@ -23,29 +23,62 @@ public class Harpoon : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        //Debug.Log("Harpoon zderzy� si� z: " + collision.gameObject.name);
-
-        // Sprawd�, czy harpoonRb nie jest kinetyczny, zanim ustawisz pr�dko��
         if (!harpoonRb.isKinematic)
         {
             // Zatrzymaj ruch harpunu
-            harpoonRb.linearVelocity = Vector3.zero;
+            harpoonRb.velocity = Vector3.zero;
             harpoonRb.angularVelocity = Vector3.zero;
         }
 
         harpoonRb.isKinematic = true;
 
-        // Przeka� dane do HarpoonController
+        // Przekaż dane do HarpoonController
         if (harpoonController != null)
         {
             harpoonController.OnHarpoonCollision();
         }
 
-        //// Je�li harpoon zderzy� si� z obiektem z tagiem "Treasure", nadaj mu dodatkow� pr�dko��
-        //if (collision.gameObject.CompareTag("Treasure"))
-        //{
-        //    harpoonRb.isKinematic = false;
-        //    harpoonRb.velocity = transform.forward * spawnedObjectSpeed;
-        //}
+        // Jeśli harpoon zderzył się z obiektem z tagiem "Treasure"
+        if (collision.gameObject.CompareTag("Treasure"))
+        {
+            // Zapisz pozycję i rotację oryginalnego obiektu
+            Vector3 originalPosition = collision.transform.position;
+            Quaternion originalRotation = collision.transform.rotation;
+
+            // Zapisz oryginalny obiekt
+            GameObject originalObject = collision.gameObject;
+
+            // Utwórz kopię obiektu bez skryptów
+            GameObject treasureCopy = Instantiate(originalObject, originalPosition, originalRotation);
+
+            //// Usuń wszystkie skrypty poza SpecificScript
+            //foreach (var script in treasureCopy.GetComponents<MonoBehaviour>())
+            //{
+            //    if (!(script is SpecificScript))
+            //    {
+            //        Destroy(script);
+            //    }
+            //}
+
+            // Ustaw obiekt jako kinematyczny, wyłącz collider i zresetuj jego skalę
+            Rigidbody treasureRb = treasureCopy.GetComponent<Rigidbody>();
+            if (treasureRb != null)
+            {
+                treasureRb.isKinematic = true;
+            }
+            Collider treasureCollider = treasureCopy.GetComponent<Collider>();
+            if (treasureCollider != null)
+            {
+                treasureCollider.enabled = false;
+            }
+            treasureCopy.transform.localScale = new Vector3(1, 1, 1);
+
+            // Przypisz kopię jako dziecko treasureMountPoint
+            treasureCopy.transform.SetParent(treasureMountPoint);
+            treasureCopy.transform.localPosition = Vector3.zero; // Ustaw lokalną pozycję na (0,0,0)
+
+            // Zniszcz oryginalny obiekt
+            Destroy(originalObject);
+        }
     }
 }
