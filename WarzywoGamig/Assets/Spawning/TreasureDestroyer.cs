e29be51dcbef1201365eb6c53baf857567500f61
CrystalHareSign <wojtek123b@onet.eu>
2025-03-18 21:07:59 +0100

    Obstacles FIX

---
+++ WarzywoGamig/Assets/Spawning/TreasureDestroyer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TreasureDestroyer : MonoBehaviour
+{
+    // Funkcja wywo³ywana, gdy inny obiekt wchodzi w kolizjê z tym obiektem
+    private void OnCollisionEnter(Collision collision)
+    {
+        // SprawdŸ, czy obiekt, z którym kolidujemy, ma tag "Wheel"
+        if (collision.gameObject.CompareTag("Wheel"))
+        {
+            // Zniszcz obiekt, na którym znajduje siê skrypt
+            Destroy(gameObject);
+            Debug.Log("Obiekt zosta³ zniszczony, poniewa¿ wszed³ w kolizjê z obiektem o tagu 'Wheel'");
+        }
+    }
+}
