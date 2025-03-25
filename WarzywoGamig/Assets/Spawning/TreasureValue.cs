8050fa94e3ee00c5a8a47c772dc1ab82b3102182
CrystalHareSign <wojtek123b@onet.eu>
2025-03-25 22:00:51 +0100

    Refiner v3.6

--- WarzywoGamig/Assets/Spawning/TreasureValue.cs
+++ WarzywoGamig/Assets/Spawning/TreasureValue.cs
@@ -4,4 +4,10 @@ public class TreasureValue : MonoBehaviour
 {
     public string category;
     public float amount;
+
+    void Awake()
+    {
+        // U¿ycie DontDestroyOnLoad na tym obiekcie, aby nie zosta³ zniszczony po zmianie sceny
+        DontDestroyOnLoad(gameObject);
+    }
 }
