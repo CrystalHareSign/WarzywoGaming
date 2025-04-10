5fba971a1beb522f3ff86f4a13e703d3621e9d49
CrystalHareSign <wojtek123b@onet.eu>
2025-04-10 22:57:48 +0200

    AudioManager FIX 3

--- WarzywoGamig/Assets/Menu/OptionsMenu.cs
+++ WarzywoGamig/Assets/Menu/OptionsMenu.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro; // Import TextMeshPro namespace
 
 public class OptionsMenu : MonoBehaviour
 {
@@ -8,14 +9,27 @@ public class OptionsMenu : MonoBehaviour
     public Slider sfxVolumeSlider;
     public Slider ambientVolumeSlider;
 
+    [Header("Percentage Texts (TextMeshPro)")]
+    public TMP_Text musicVolumeText;
+    public TMP_Text sfxVolumeText;
+    public TMP_Text ambientVolumeText;
+
     private void Start()
     {
+        // Ustaw zakres wartoœci sliderów na 0-100
+        musicVolumeSlider.minValue = 0;
+        musicVolumeSlider.maxValue = 100;
+        sfxVolumeSlider.minValue = 0;
+        sfxVolumeSlider.maxValue = 100;
+        ambientVolumeSlider.minValue = 0;
+        ambientVolumeSlider.maxValue = 100;
+
         LoadCurrentSettings();
 
         // Dodaj s³uchaczy do suwaków
-        musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
-        sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
-        ambientVolumeSlider.onValueChanged.AddListener(SetAmbientVolume);
+        musicVolumeSlider.onValueChanged.AddListener(delegate { UpdateSliderValue(musicVolumeSlider, musicVolumeText, SetMusicVolume); });
+        sfxVolumeSlider.onValueChanged.AddListener(delegate { UpdateSliderValue(sfxVolumeSlider, sfxVolumeText, SetSFXVolume); });
+        ambientVolumeSlider.onValueChanged.AddListener(delegate { UpdateSliderValue(ambientVolumeSlider, ambientVolumeText, SetAmbientVolume); });
 
         // Obs³uga braku AudioManager.Instance
         if (AudioManager.Instance == null)
@@ -25,6 +39,19 @@ public class OptionsMenu : MonoBehaviour
             sfxVolumeSlider.interactable = false;
             ambientVolumeSlider.interactable = false;
         }
+    }
+
+    private void UpdateSliderValue(Slider slider, TMP_Text text, System.Action<float> updateAction)
+    {
+        // Zaokr¹glenie wartoœci suwaka do liczby ca³kowitej
+        float roundedValue = Mathf.Round(slider.value);
+        slider.value = roundedValue;
+
+        // Aktualizacja tekstu procentowego
+        text.text = $"{(int)roundedValue}%";
+
+        // Wywo³anie odpowiedniej akcji (np. ustawienie g³oœnoœci)
+        updateAction?.Invoke(roundedValue / 100f); // Przekazujemy wartoœæ w zakresie 0-1 do AudioManager
     }
 
     public void SetMusicVolume(float volume)
@@ -56,9 +83,9 @@ public class OptionsMenu : MonoBehaviour
         // Zapisanie zmian w AudioManagerze oraz PlayerPrefs
         if (AudioManager.Instance != null)
         {
-            PlayerPrefs.SetFloat("MusicVolume", musicVolumeSlider.value);
-            PlayerPrefs.SetFloat("SFXVolume", sfxVolumeSlider.value);
-            PlayerPrefs.SetFloat("AmbientVolume", ambientVolumeSlider.value);
+            PlayerPrefs.SetFloat("MusicVolume", musicVolumeSlider.value / 100f);
+            PlayerPrefs.SetFloat("SFXVolume", sfxVolumeSlider.value / 100f);
+            PlayerPrefs.SetFloat("AmbientVolume", ambientVolumeSlider.value / 100f);
             PlayerPrefs.Save();
 
             Debug.Log("Zmiany g³oœnoœci zosta³y zastosowane i zapisane!");
@@ -84,14 +111,14 @@ public class OptionsMenu : MonoBehaviour
             float savedSFXVolume = PlayerPrefs.HasKey("SFXVolume") ? PlayerPrefs.GetFloat("SFXVolume") : AudioManager.Instance.masterSFXVolume;
             float savedAmbientVolume = PlayerPrefs.HasKey("AmbientVolume") ? PlayerPrefs.GetFloat("AmbientVolume") : AudioManager.Instance.masterAmbientVolume;
 
-            musicVolumeSlider.value = savedMusicVolume;
-            sfxVolumeSlider.value = savedSFXVolume;
-            ambientVolumeSlider.value = savedAmbientVolume;
+            // Przekszta³cenie wartoœci 0-1 na 0-100
+            musicVolumeSlider.value = savedMusicVolume * 100f;
+            sfxVolumeSlider.value = savedSFXVolume * 100f;
+            ambientVolumeSlider.value = savedAmbientVolume * 100f;
 
-            // Aktualizacja AudioManagera
-            AudioManager.Instance.SetMusicVolume(savedMusicVolume);
-            AudioManager.Instance.SetSFXVolume(savedSFXVolume);
-            AudioManager.Instance.SetAmbientVolume(savedAmbientVolume);
+            UpdateSliderValue(musicVolumeSlider, musicVolumeText, SetMusicVolume);
+            UpdateSliderValue(sfxVolumeSlider, sfxVolumeText, SetSFXVolume);
+            UpdateSliderValue(ambientVolumeSlider, ambientVolumeText, SetAmbientVolume);
         }
         else
         {
