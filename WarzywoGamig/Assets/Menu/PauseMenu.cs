a455c0ce969c26dfc9b5c9081e077f3b771b429f
CrystalHareSign <wojtek123b@onet.eu>
2025-06-29 19:39:09 +0200

    NEW INVENTORY !

--- WarzywoGamig/Assets/Menu/PauseMenu.cs
+++ WarzywoGamig/Assets/Menu/PauseMenu.cs
@@ -68,6 +68,8 @@ public class PauseMenu : MonoBehaviour
     void Update()
     {
         if (InputBlocker.Active) return;
+        // Blokada menu pauzy gdy inventory otwarte
+        if (InventoryGridManager.InventoryOpen) return;
         // Blokuj pauzê jeœli trwa dialog
         if (DialogueManager.DialogueActive) return;
         // Dodanie warunku, który sprawdza, czy menu jest dostêpne
